a4515b14ce9aa040f1722c8f381d3ab4c53341b2
rhianpablo11 <rhianpablo11@gmail.com>
2026-05-07 12:05:46 -0300

    ci: setup ambient of dev

---
+++ rhizome_app/src/pages/home.tsx
@@ -0,0 +1,12 @@
+
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Rhizome</h1>
+      <p>Your personal knowledge management system.</p>
+    </div>
+  );
+}
+
+export default Home;
