676d6b1908260c9f50e92eca190b4bef94fedc51
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-09 13:36:28 -0300

    refactor: add main menu semantic html tags

--- rhizome_app/src/pages/MainMenu/MainMenu.tsx
+++ rhizome_app/src/pages/MainMenu/MainMenu.tsx
@@ -1,11 +1,22 @@
-
+import styles from "./MainMenu.module.css"
 
 function MainMenu() {
   return (
-    <div className="container">
-      <h1>Rhizome</h1>
-      <p>A rota do sistema!</p>
-    </div>
+    <main className={styles.container}>
+      <div className={styles.containerWrapper}>
+        <section className={styles.contentWrapper}>
+          <header className={styles.header}>
+            <h1>Rhizome</h1>
+            <p>A rota do sistema!</p>
+          </header>
+
+          <div className={styles.btnWrapper}>
+            <button>Novo Jogo</button>
+          </div>
+
+        </section>
+      </div>
+    </main>
   );
 }
 
