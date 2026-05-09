dfd012aee138292011e3def7aaddea1b3a49b145
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-09 14:55:15 -0300

    feat: add common navbar

--- rhizome_app/src/main.tsx
+++ rhizome_app/src/main.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home'
 import BaseLayout from './pages/BaseLayout/BaseLayout'
 import MainMenu from './pages/MainMenu/MainMenu'
+import Navbar from './pages/Navbar/Navbar'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <BrowserRouter>
           <Routes>
             <Route path="/" element={<BaseLayout />}>
-              <Route index element={<MainMenu />}/>
+              <Route path="/" element={<Navbar />}>
+                <Route index element={<MainMenu />}/>
+                <Route path="/home" element={<Home />}/>
+              </Route>
             </Route>
           </Routes>
       </BrowserRouter>
