02c910238dd843fa62ded4fbbc24ca8cf84bb722
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-09 14:26:10 -0300

    feat: add base layout to main menu page

--- rhizome_app/src/main.tsx
+++ rhizome_app/src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home'
 import BaseLayout from './pages/BaseLayout/BaseLayout'
+import MainMenu from './pages/MainMenu/MainMenu'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <BrowserRouter>
           <Routes>
             <Route path="/" element={<BaseLayout />}>
-              <Route index element={<Home />}/>
+              <Route index element={<MainMenu />}/>
             </Route>
           </Routes>
       </BrowserRouter>
--- rhizome_app/src/pages/MainMenu/MainMenu.tsx
+++ rhizome_app/src/pages/MainMenu/MainMenu.tsx
@@ -2,21 +2,17 @@ import styles from "./MainMenu.module.css"
 
 function MainMenu() {
   return (
-    <main className={styles.container}>
-      <div className={styles.containerWrapper}>
-        <section className={styles.contentWrapper}>
-          <header className={styles.header}>
-            <h1>Rhizome</h1>
-            <p>A rota do sistema!</p>
-          </header>
+    <section className={styles.contentWrapper}>
+      <header className={styles.header}>
+        <h1>Rhizome</h1>
+        <p>A rota do sistema!</p>
+      </header>
 
-          <div className={styles.btnWrapper}>
-            <button>Novo Jogo</button>
-          </div>
-
-        </section>
+      <div className={styles.btnWrapper}>
+        <button>Novo Jogo</button>
       </div>
-    </main>
+
+    </section>
   );
 }
 
