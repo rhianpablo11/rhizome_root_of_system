667ebd4cc2e93fc4b751d7d6916962b9705c7277
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-09 14:14:09 -0300

    feat: add react router structure

--- rhizome_app/src/main.tsx
+++ rhizome_app/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home'
+import BaseLayout from './pages/BaseLayout/BaseLayout'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+    <StrictMode>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<BaseLayout />}>
+              <Route index element={<Home />}/>
+            </Route>
+          </Routes>
+      </BrowserRouter>
+    </StrictMode>
 )
