ccddcd3a76f6147924f098335b0bdf87fc7c48a6
rhianpablo11 <rhianpablo11@gmail.com>
2026-05-10 13:04:54 -0300

    feat: add Button component and integrate into player selection page

---
+++ rhizome_app/src/components/button.tsx
@@ -0,0 +1,16 @@
+import type { IButton } from "../interfaces/components/IButton"
+
+
+function Button(props: IButton){
+    const {text, color} = props
+
+    return(
+        <>
+            <button className={`${color == 'salmon' ? 'bg-[#FF7E67]' : 'bg-[#1E293B]'} w-full h-13 rounded-4xl text-white font-semibold text-xl flex items-center justify-center`}>
+                {text}
+            </button>
+        </>
+    )
+}
+
+export default Button
--- rhizome_app/src/pages/selectNameOfPlayers.tsx
+++ rhizome_app/src/pages/selectNameOfPlayers.tsx
@@ -1,28 +1,36 @@
+import Button from "../components/button";
 import Input from "../components/input";
 
 function SelectNameOfPlayers() {
     return (
-        <div className="flex flex-col gap-y-3">
-            <Input
-                usesOn="selectNameOfPlayers"
-                placeholder="Jogador 1"
-            />
-            <Input
-                usesOn="selectNameOfPlayers"
-                placeholder="Jogador 2"
-            />
-            <Input
-                usesOn="selectNameOfPlayers"
-                placeholder="Jogador 3"
-            />
-            <Input
-                usesOn="selectNameOfPlayers"
-                placeholder="Jogador 4"
-            />
-            <Input
-                usesOn="selectNameOfPlayers"
-                placeholder="Jogador 5"
-            />
+        <div className="w-full flex flex-col  mt-7">
+            <div className="flex flex-col overflow-y-auto gap-y-3">
+                <Input
+                    usesOn="selectNameOfPlayers"
+                    placeholder="Jogador 1"
+                />
+                <Input
+                    usesOn="selectNameOfPlayers"
+                    placeholder="Jogador 2"
+                />
+                <Input
+                    usesOn="selectNameOfPlayers"
+                    placeholder="Jogador 3"
+                />
+                <Input
+                    usesOn="selectNameOfPlayers"
+                    placeholder="Jogador 4"
+                />
+                <Input
+                    usesOn="selectNameOfPlayers"
+                    placeholder="Jogador 5"
+                />
+                
+            </div>
+            <div className="flex flex-col w-full gap-y-2 fixed bottom-4 left-0 px-5">
+                <Button color="darkBlue" text="Adicionar Jogador" />
+                <Button color="salmon" text="Iniciar Batalha" />
+            </div>
         </div>
     );
 }
