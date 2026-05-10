89dc055a7137f957152ed911b819f210efd1a0ee
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-10 18:40:19 -0300

    feat: add new base layout

--- rhizome_app/src/pages/BaseLayout/BaseLayout.tsx
+++ rhizome_app/src/pages/BaseLayout/BaseLayout.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router";
-import styles from "./BaseLayout.module.css";
+import bg from "../../assets/bg.png";
+import LogoType from "../../components/logoType";
 
 function BaseLayout() {
     return (
-        <div className={styles.container}>
-            <div className={styles.containerWrapper}>
-                <Outlet />
+        <>
+            <div
+                className="w-full h-dvh overflow-hidden bg-cover bg-no-repeat"
+                style={{
+                    backgroundImage: `url("${bg}")`,
+                }}>
+                <div className="w-full h-full flex flex-col backdrop-blur-[3px]">
+                    <div className="shrink-0">
+                        <LogoType localOfUse="offlinePage" />
+                    </div>
+                    <Outlet />
+                </div>
             </div>
-        </div>
+        </>
+
     );
 }
 
