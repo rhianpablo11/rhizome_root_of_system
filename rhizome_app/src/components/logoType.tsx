c54c0d3acdb6f72539c54641bc606e3b7fee8055
rhianpablo11 <rhianpablo11@gmail.com>
2026-05-10 11:07:14 -0300

    feat: added logoType component

---
+++ rhizome_app/src/components/logoType.tsx
@@ -0,0 +1,14 @@
+import type { ILogoType } from "../interfaces/components/ILogoType";
+
+
+function LogoType(props: ILogoType) {
+    const { localOfUse } = props;
+    return (
+        <div className='flex flex-col items-center m-0 p-0'>
+            <h1 className={`${ localOfUse === 'offlinePage' ? 'text-[80px] mt-6 leading-10' : 'text-[60px]' } font-hand text-[#1E293B]`}>Rhizome</h1>
+            <h1 className={`${ localOfUse === 'offlinePage' ? 'text-base' : 'text-lg ' } text-[#1E293B] font-light`}>A rota do sistema</h1>
+        </div>
+    );
+}
+
+export default LogoType;
