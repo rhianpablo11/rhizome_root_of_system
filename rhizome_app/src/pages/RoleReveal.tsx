1e07798ce66db880882f7722d3ee15668af13f7f
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-10 19:28:47 -0300

    feat: add page responsivity

--- rhizome_app/src/pages/RoleReveal.tsx
+++ rhizome_app/src/pages/RoleReveal.tsx
@@ -3,12 +3,14 @@ import Button from "../components/button";
 
 function RoleReveal() {
     return (
-        <div className="w-full h-full flex flex-col items-center justify-center">
-            <div className="w-[360px] h-[540px] rounded-2xl overflow-hidden">
+        <div className="w-full h-full flex flex-col items-center justify-between py-4">
+             <div /> {/* pra o card ficar centralizado verticalmente */}
+
+            <div className=" w-[min(80vw,50vh)] max-h-[960px] rounded-2xl overflow-hidden">
                 <img src={card} alt="" className="w-full h-full object-cover"/>
             </div>
 
-            <div className="flex w-full gap-y-2 fixed bottom-4 left-0 px-10">
+            <div className="flex w-full gap-y-2 bottom-4 left-0 px-10">
                 <Button 
                     onClickButtonChildren={() => {}}
                     color="salmon"
