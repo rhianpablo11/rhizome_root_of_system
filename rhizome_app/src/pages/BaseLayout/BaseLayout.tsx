9fa3d331952068ad367420c6a34082e716d58f58
rhianpablo11 <rhianpablo11@gmail.com>
2026-05-10 11:53:18 -0300

    update: format code with npm run format code
    - the comands "npm run format" to set code unified style

--- rhizome_app/src/pages/BaseLayout/BaseLayout.tsx
+++ rhizome_app/src/pages/BaseLayout/BaseLayout.tsx
@@ -1,15 +1,20 @@
 import { Outlet } from "react-router";
-import styles from "./BaseLayout.module.css"
-
+import styles from "./BaseLayout.module.css";
 
 function BaseLayout() {
-  return (
-    <div className={styles.container}>
-        <div className={styles.containerWrapper}>
-            <Outlet />
+    return (
+        <div
+            className={styles.container}
+        >
+            <div
+                className={
+                    styles.containerWrapper
+                }
+            >
+                <Outlet />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default BaseLayout;
--- rhizome_app/src/pages/MainMenu/MainMenu.tsx
+++ rhizome_app/src/pages/MainMenu/MainMenu.tsx
@@ -1,19 +1,34 @@
-import styles from "./MainMenu.module.css"
+import styles from "./MainMenu.module.css";
 
 function MainMenu() {
-  return (
-    <section className={styles.contentWrapper}>
-      <header className={styles.header}>
-        <h1>Rhizome</h1>
-        <p>A rota do sistema!</p>
-      </header>
+    return (
+        <section
+            className={
+                styles.contentWrapper
+            }
+        >
+            <header
+                className={
+                    styles.header
+                }
+            >
+                <h1>Rhizome</h1>
+                <p>
+                    A rota do sistema!
+                </p>
+            </header>
 
-      <div className={styles.btnWrapper}>
-        <button >Novo Jogo</button>
-      </div>
-
-    </section>
-  );
+            <div
+                className={
+                    styles.btnWrapper
+                }
+            >
+                <button>
+                    Novo Jogo
+                </button>
+            </div>
+        </section>
+    );
 }
 
 export default MainMenu;
