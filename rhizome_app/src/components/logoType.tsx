e6495dbfff41164d0bd2df629574a3d90b9daba4
rhianpablo11 <rhianpablo11@gmail.com>
2026-05-10 11:11:03 -0300

    update: update font size on logoType component

--- rhizome_app/src/components/logoType.tsx
+++ rhizome_app/src/components/logoType.tsx
@@ -5,8 +5,8 @@ function LogoType(props: ILogoType) {
     const { localOfUse } = props;
     return (
         <div className='flex flex-col items-center m-0 p-0'>
-            <h1 className={`${ localOfUse === 'offlinePage' ? 'text-[80px] mt-6 leading-10' : 'text-[60px]' } font-hand text-[#1E293B]`}>Rhizome</h1>
-            <h1 className={`${ localOfUse === 'offlinePage' ? 'text-base' : 'text-lg ' } text-[#1E293B] font-light`}>A rota do sistema</h1>
+            <h1 className={`${ localOfUse == 'offlinePage' ? 'text-[80px] mt-6 leading-10' : 'text-[100px] mt-8 leading-14' } font-hand text-[#1E293B]`}>Rhizome</h1>
+            <h1 className={`${ localOfUse == 'offlinePage' ? 'text-base' : 'text-lg ' } text-[#1E293B] font-light`}>A rota do sistema</h1>
         </div>
     );
 }
