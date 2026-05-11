026b1fa8edfb3d6272120ce4d846956698aaf5c5
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-11 15:09:22 -0300

    feat: add card description scroll

--- rhizome_app/src/components/cardModal.tsx
+++ rhizome_app/src/components/cardModal.tsx
@@ -5,10 +5,6 @@ import type { ICardModal } from "../interfaces/components/ICardModal";
 function CardModal(props: ICardModal) {
     const { title, description, children } = props;
 
-    const handleButtonClickFather = () => {
-        console.log("clicado");
-    };
-
     return (
         <>
             <div className="absolute z-1000 inset-0 bg-black/40 backdrop-blur-sm"></div>
@@ -16,12 +12,17 @@ function CardModal(props: ICardModal) {
             <div className="absolute z-1001 flex justify-center items-center w-full h-full px-8">
                 <div className="relative flex flex-col justify-center items-center w-full max-w-[400px]">
                     <img src={cardInfo} alt=""/>
-                    <div className="absolute flex flex-col items-center h-[40%] top-[40%] w-[90%] mx-16 ">
+                    <div className="absolute flex flex-col items-center h-[41%] top-[40%] w-[90%] mx-16 ">
                         <h1 className="text-[#1E293B] font-medium text-2xl [@media(min-width:400px)]:text-4xl pt-5">{title}</h1>
-                        <p className="text-[#1E293B] font-medium text-sm [@media(min-width:400px)]:text-base pt-1 px-2 overflow-y-auto">{description}</p>
+                        <div className="relative px-2 overflow-y-auto h-[75%]">
+                            <p className="text-[#1E293B] font-medium text-sm [@media(min-width:400px)]:text-base pt-1">
+                                {description}
+                            </p>
+                        </div>
+
                     </div>
 
-                    <div className="absolute flex items-center justify-center gap-2 top-[83%] px-4 w-[90%] h-[15%]">
+                    <div className="absolute flex items-center justify-center gap-2 top-[84%] px-4 w-[90%] h-[13.5%]">
                         {children} 
                     </div>
                 </div>   
