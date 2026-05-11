7beb2f8279d826033531420f6cb743dbcec2c805
rhianpablo11 <rhianpablo11@gmail.com>
2026-05-11 09:11:03 -0300

    feat: add HeaderGamingPoints component and IHeaderGamingPoints interface
    - this component show in header during game execution

---
+++ rhizome_app/src/components/headerGamingPoints.tsx
@@ -0,0 +1,13 @@
+import type { IHeaderGamingPoints } from "../interfaces/components/IHeaderGamingPoints"
+
+
+function HeaderGamingPoints(props: IHeaderGamingPoints){
+    const {pointsCommunity, pointsLobby, idSession} = props
+    
+    return(
+        <>
+        </>
+    )
+}
+
+export default HeaderGamingPoints
